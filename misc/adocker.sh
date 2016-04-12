202313f525bd715d544ff874390a0555f57b21fa
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-04-12 11:24:12 +0200

    [ACPDOC-4] improve start/stop script

--- misc/adocker.sh
+++ misc/adocker.sh
@@ -10,15 +10,34 @@ case "$1" in
            echo "[WARNING] It will not be abble to map all docker container processes and connections
             on this OS and may have some problems with the logs..."
         fi
+        already_started=`ps -aef | grep 'ariane_docker' | grep -v grep | awk '{print $2}' | wc -l`
+        if [ $already_started -eq 1 ]; then
+            echo "[ERROR] Ariane Docker plugin is already started on this server."
+            exit
+        fi
         echo "Starting Ariane Docker"
-        python3 -m ariane_docker &
-        echo $! > /tmp/adocker.pid
+        nohup python3 -m ariane_docker > /var/log/ariane/adocker_nohup.log 2>&1 &
         ;;
     stop)
-        if [ -f /tmp/adocker.pid ]; then
-            echo "Stopping Ariane Docker"
-            cat /tmp/adocker.pid | xargs kill
-            rm /tmp/adocker.pid
+        stopped=`ps -aef | grep 'ariane_docker' | grep -v grep | awk '{print $2}' | wc -l`
+        if [ $stopped -eq 1 ]; then
+            printf "Stopping Ariane Docker "
+            pid=`ps -aef | grep 'ariane_docker' | grep -v grep | awk '{print $2}' `
+            kill $pid
+
+            count=0
+            stopped=`ps -aef | grep 'ariane_docker' | grep -v grep | awk '{print $2}' | wc -l`
+            while [ $stopped -eq 1 ]
+            do
+                count=`expr $count + 1`
+                if [ $count -eq 10 ]; then
+                    kill -9 $pid
+                fi
+                sleep 1
+                printf "."
+                stopped=`ps -aef | grep 'ariane_docker' | grep -v grep | awk '{print $2}' | wc -l`
+            done
+            printf "\nAriane Docker is stopped.\n"
         else
             echo "Ariane Docker not started..."
         fi
